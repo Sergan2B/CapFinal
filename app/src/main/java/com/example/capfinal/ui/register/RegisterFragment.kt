5627dcdab4290aa14aa142bdc4d4789bb8436752
Aruuke7 <alimbekovaar@gmail.com>
2022-05-24 23:42:44 +0600

    created SortFragment and RegisterFragment

---
+++ app/src/main/java/com/example/capfinal/ui/register/RegisterFragment.kt
@@ -0,0 +1,21 @@
+package com.example.capfinal.ui.register
+
+
+import android.view.LayoutInflater
+import com.example.capfinal.core.base.BaseFragment
+import kg.geektech.finalprojectcustomcap.databinding.FragmentRegisterBinding
+
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
+    override fun inflateVB(inflater: LayoutInflater): FragmentRegisterBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListener() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/capfinal/ui/sort/SortFragment.kt
@@ -0,0 +1,20 @@
+package com.example.capfinal.ui.sort
+
+import android.view.LayoutInflater
+import com.example.capfinal.core.base.BaseFragment
+import kg.geektech.finalprojectcustomcap.databinding.FragmentSortBinding
+
+class SortFragment : BaseFragment<FragmentSortBinding>() {
+    override fun inflateVB(inflater: LayoutInflater): FragmentSortBinding {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListener() {
+        TODO("Not yet implemented")
+    }
+
+}
